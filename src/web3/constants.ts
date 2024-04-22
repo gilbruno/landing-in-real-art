1ed196a137156f876f0a921c72481c48350a5821
Gilles Bruno <gilles_bruno@gmx.fr>
2024-04-22 16:36:25 +0200

    Modify sc address

--- src/web3/constants.ts
+++ src/web3/constants.ts
@@ -1,3 +1,3 @@
-export const orderPhygitalArtAddress = "0xa546D99E5DE183CC0057CaeD71D5DFf03580B2c3"
+export const orderPhygitalArtAddress = "0xc74AAA71A67327BFE74983dA06845bd94F8EcFcF"
 export const usdtAddress = "0xEdDa4460fCCF59420D18fD580738BcAa30d31D02"
 export const USDT_DECIMALS = 6
