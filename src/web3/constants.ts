86f4ea26565869526e547a9903e74864cac911b3
Gilles Bruno <gilles_bruno@gmx.fr>
2024-04-23 10:07:29 +0200

    Change address SC after deployment

--- src/web3/constants.ts
+++ src/web3/constants.ts
@@ -1,3 +1,3 @@
-export const orderPhygitalArtAddress = "0xc74AAA71A67327BFE74983dA06845bd94F8EcFcF"
+export const orderPhygitalArtAddress = "0x75ca4bB1e2b2e125D195C5a02b7f855149f7D07C"
 export const usdtAddress = "0xEdDa4460fCCF59420D18fD580738BcAa30d31D02"
 export const USDT_DECIMALS = 6
